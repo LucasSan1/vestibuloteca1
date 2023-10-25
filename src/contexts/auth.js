70cdcc947d0c0849f9284021490f1fca349059d5
LucasSan1 <137550683+LucasSan1@users.noreply.github.com>
2023-10-25 14:41:29 -0300

    Update auth.js

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
         localStorage.removeItem("user");
         localStorage.removeItem("token");
         setUser(null);
-        Navigate("/login");
+        Navigate("/");
     };
 
     // Antes da página ser renderizada, verifica se o usúario ainda esta logado no localStorage  
